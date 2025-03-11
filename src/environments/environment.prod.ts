1c856407d8b36d21838c852ada460167f172642e
Parag Jain <iesparagjain@gmail.com>
2025-03-11 15:59:27 +0530

    BAckend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     // apiBaseUrl: 'https://e-commerce-backend-two-delta.vercel.app/api/v1',
-    apiBaseUrl: 'https://e-commerce-by-r9f5lau5y-iesparagjaingmailcoms-projects.vercel.app/api/v1',
+    apiBaseUrl: 'https://e-commerce-by-pj.vercel.app/api/v1',
     stripePublishableKey:'pk_test_51QkPqnSAOZ17nUFXP7NY9335jNIEaoHpxxn3tt7eAwUzBc5vwAIerYQQQ4VXjviogaPMlGYLuIGy521J5u4O0b3000IXx2plVb'
 };
